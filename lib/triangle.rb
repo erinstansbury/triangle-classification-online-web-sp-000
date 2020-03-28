8d5556d1c15156be9b85f98118b17e6df124b423
Erin Stansbury <ekstansbury@gmail.com>
2020-03-28 02:43:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
     if self.side1 == 0 || self.side2 == 0 || self.side3 == 0
       raise TriangleError
     elsif self.side1 == self.side2 && self.side2 == self.side3
-      puts "equilateral"
+      :equilateral
     end
   end
 
